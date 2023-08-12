6de4edc9faebab982972d5982f06e536bb0d9828
Annie Ulanska <a.ulanskaxxi@gmail.com>
2023-08-12 20:18:37 +0100

    Readme changed in pl

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+    // add images to li elements
+const galleryImages = galleryItems.map((img) => 
+`<li>
+<a class="gallery__item" href="${img.original}">
+<img
+      class="gallery__image"
+      src="${img.preview}"
+      alt="${img.description}"
+    /></a>
+    </li>`).join("");
+
+    // add array of imgs to gallery
+gallery.insertAdjacentHTML("afterbegin", galleryImages);
+
+// SimpleLightbox.com
+let simplelightbox = new SimpleLightbox('.gallery a', {
+    /* options */
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    showCounter: true,
+    captionDelay: 250,
+  });
