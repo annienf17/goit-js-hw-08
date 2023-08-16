f7b25153b8d67272f9780ed0a584055fa773e3e2
Annie Ulanska <a.ulanskaxxi@gmail.com>
2023-08-16 16:39:51 +0100

    vimeo player

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,6 +2,7 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+console.log(SimpleLightbox);
 
 const gallery = document.querySelector(".gallery");
 
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,41 @@
+// task 7
+import throttle from 'lodash.throttle';
+import Vimeo from '@vimeo/player';
+
+// task 5 & task 7
+const vpCurrentTime = "videoplayer-current-time";
+const refreshTime = 1000; //current time update every second
+
+// task 3 - vimeo player initialization
+const findVimeoPlayer = document.querySelector("#vimeo-player");
+const player = new Vimeo(findVimeoPlayer);
+
+// console log title of movie
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+// task 5 & 6 - player initialization & current time in local sorage
+player.setCurrentTime(Math.round(localStorage.getItem(vpCurrentTime)))
+// seconds = the actual time that the player seeked to
+.then(seconds => console.log(`seconds: ${seconds} = the actual time that the player seeked to`))
+// the time was less than 0 or greater than the video’s duration
+.catch(error => console.log(`${error} the time was less than 0 or greater than the video’s duration`));
+
+// on(event: string, callback: function): void
+const onPlay = timeUpdate => {
+    player.getEnded().then(ended => {
+        // ended = whether or not the video has ended
+        if(ended) {
+            console.log(`The video has ended`);
+            localStorage.setItem(vpCurrentTime, 0)
+        } else {
+            console.log("The video is being played");
+            localStorage.setItem(vpCurrentTime, timeUpdate.seconds)
+        }
+    });
+}
+// Events  *** task 7
+player.on('timeupdate', throttle(onPlay, refreshTime));
+
 
